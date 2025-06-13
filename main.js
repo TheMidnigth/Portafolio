81717fa9d9e9a58c9807bd5fb0f034cdd3b877bc
TheMidnight <reyeskeyner110@gmail.com>
2025-06-13 14:45:21 -0500

    agregando al section inicio contador de experencia,commit ,tecnologias usadas y proyectos terminados

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const buttons = document.querySelectorAll(".box__button");
-    const sections = document.querySelectorAll(".detalles__resumen");
+    //Variables
+    const buttons = document.querySelectorAll(".box__button"); //Seleccionamos todos los botones con la clase .box__button
+    const sections = document.querySelectorAll(".detalles__resumen"); //Seleccionamos todos los botones con la clase .detalles__resumen
 
     // Mostrar "Experiencia" al cargar
     showSection("experiencia");
 
+    //Recorrecomes todos los botones y les aplicamos el evento addEventListener click
     buttons.forEach((button) => {
         button.addEventListener("click", () => {
             const texto = button.textContent.trim().toLowerCase().replace(/\s+/g, ''); // experiencia, educacion, etc.
